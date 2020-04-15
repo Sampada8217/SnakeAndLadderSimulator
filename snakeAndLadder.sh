4d08b2860569f83e00fe62d0b8a054a42f3ead67
sampada8217 <sampadatirthkar@gmail.com>
2020-04-15 23:00:20 +0530

    Added Code for Check a Option and Refactored Code

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to Snake and Ladder Game"
-startPosition=0
+START_POS=0
+position=0
 rollsDie=$(( (RANDOM%6) +1 ))
-
+function  checkForOption()
+{
+     option=$(( (RANDOM%3) +1 ))
+     case $option in
+     1)echo "No play stays in same positon"
+       positon=0    ;;
+     2)position=$(( $position + $rollsDie )) ;;
+     3)position=$(( $rollDie - $position ))  ;;
+     esac
+}
+checkPosition="$( checkForOption )"
 
