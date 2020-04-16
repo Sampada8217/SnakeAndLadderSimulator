08a5dcec113bac1a18b666766f6377e073aa527d
sampada8217 <sampadatirthkar@gmail.com>
2020-04-16 13:11:41 +0530

    Added Code for game with 2 player and winner of game

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,31 +1,64 @@
 #!/bin/bash -x
 echo "Welcome to Snake and Ladder Game"
 WIN_POS=100
-position=0
-count=0
+position1=0
+position2=0
+count1=0
+count2=0
 function  checkForOption()
 {
-     while [ $position -ne $WIN_POS ]
+     while [ $position1 -ne $WIN_POS ] || [ $position2 -ne $WIN_POS ] 
      do
-        rollsDie=$(( (RANDOM%6) +1 ))
+        echo "Player 1 "
+        rollsDie1=$(( (RANDOM%6) +1 ))
         ((count++))
-  	option=$(( (RANDOM%3) +1 ))
- 	case $option in
-     	1)echo $position                ;;
-     	2)position=$(( $position + $rollsDie ))
-          if [ $position -gt 100 ] 
+  	option1=$(( (RANDOM%3) +1 ))
+ 	case $option1 in
+     	1)echo $position1                ;;
+     	2)position1=$(( $position1 + $rollsDie1 ))
+          if [ $position1 -gt 100 ] 
           then
-               position=$(( $position - $rollsDie ))
-          fi
-                                  ;;
-      	3)position=$(( $position - $rollsDie ))
-         if [ $position -lt 0 ]
+               position1=$(( $position1 - $rollsDie1 ))
+          fi                                  ;;
+      	3)position1=$(( $position1 - $rollsDie1 ))
+         if [ $position1 -lt 0 ]
          then
-              position=0
-         fi          ;;
+              position1=0
+         fi                         ;;
      	esac
-     done
-echo $count
+
+        echo "Player 2"
+        rollsDie=$(( (RANDOM%6) +1 ))
+        ((count1++))
+	rollsDie2=$(( (RANDOM%6) +1 ))
+ 	((count1++))
+	option2=$(( (RANDOM%3) +1 ))
+        case $option2 in
+        1)echo $position2                     ;;
+	2)position2=$(( $position2 + $rollsDie2 ))
+          if [ $position2 -gt  $WIN_POS ]
+          then
+               position2=$(( $position2 - $rollsDie2 ))
+          fi                                ;;
+        3)position2=$(( $position2 - $rollsDie2 ))
+          if [ $position2 -lt 0 ]
+          then
+	       position2=0
+          fi                        ;;
+       esac    
+   done
+
+echo $count1
+echo $count2
+
+if [ $position1 -eq $WIN_POS ]
+then
+     echo "Player1 is Winner"
+elif [ $position2 -eq $WIN_POS ]
+then
+     echo "Player2 is Winner"
+fi
 }
-checkPosition="$( checkForOption )"
+gamePlaying="$( checkForOption ) " 
+
 
