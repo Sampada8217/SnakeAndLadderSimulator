523a459855fb9bcf0189cf85a51c26bcf0e97127
sampada8217 <sampadatirthkar@gmail.com>
2020-04-16 14:45:17 +0530

    Refactored Code(Variable name and function name)

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,64 +1,63 @@
 #!/bin/bash -x
 echo "Welcome to Snake and Ladder Game"
 WIN_POS=100
-position1=0
-position2=0
-count1=0
-count2=0
-function  checkForOption()
+posPlayer1=0
+posPlayer2=0
+diceCountPlayer1=0
+diceCountPlayer2=0
+function  snakeAndLadderGame()
 {
-     while [ $position1 -ne $WIN_POS ] || [ $position2 -ne $WIN_POS ] 
+     while [ $posPlayer1 -ne $WIN_POS ] || [ $posPlayer2 -ne $WIN_POS ] 
      do
         echo "Player 1 "
         rollsDie1=$(( (RANDOM%6) +1 ))
-        ((count++))
+        ((diceCountPlayer1++))
   	option1=$(( (RANDOM%3) +1 ))
  	case $option1 in
-     	1)echo $position1                ;;
-     	2)position1=$(( $position1 + $rollsDie1 ))
-          if [ $position1 -gt 100 ] 
+     	1)echo $posPlayer1                ;;
+     	2)posPlayer1=$(( $posPlayer1 + $rollsDie1 ))
+          if [ $posPlayer1 -gt 100 ] 
           then
-               position1=$(( $position1 - $rollsDie1 ))
+               posPlayer1=$(( $posPlayer1 - $rollsDie1 ))
           fi                                  ;;
-      	3)position1=$(( $position1 - $rollsDie1 ))
-         if [ $position1 -lt 0 ]
+      	3)posPlayer1=$(( $posPlayer1 - $rollsDie1 ))
+         if [ $posPlayer1 -lt 0 ]
          then
-              position1=0
+              posPlayer1=0
          fi                         ;;
      	esac
 
+      #From here turn switches to player2
         echo "Player 2"
-        rollsDie=$(( (RANDOM%6) +1 ))
-        ((count1++))
-	rollsDie2=$(( (RANDOM%6) +1 ))
- 	((count1++))
+        rollsDie2=$(( (RANDOM%6) +1 ))
+        ((diceCountPlayer2++))
 	option2=$(( (RANDOM%3) +1 ))
         case $option2 in
-        1)echo $position2                     ;;
-	2)position2=$(( $position2 + $rollsDie2 ))
-          if [ $position2 -gt  $WIN_POS ]
+        1)echo $posPlayer2                     ;;
+	2)posPlayer2=$(( $posPlayer2 + $rollsDie2 ))
+          if [ $posPlayer2 -gt  $WIN_POS ]
           then
-               position2=$(( $position2 - $rollsDie2 ))
+               posPlayer2=$(( $posPlayer2 - $rollsDie2 ))
           fi                                ;;
-        3)position2=$(( $position2 - $rollsDie2 ))
-          if [ $position2 -lt 0 ]
+        3)posPlayer2=$(( $posPlayer2 - $rollsDie2 ))
+          if [ $posPlayer2 -lt 0 ]
           then
-	       position2=0
+	       posPlayer2=0
           fi                        ;;
        esac    
    done
 
-echo $count1
-echo $count2
+echo $diceCountPlayer1
+echo $diceCountPlayer2
 
-if [ $position1 -eq $WIN_POS ]
+if [ $posPlayer1 -eq $WIN_POS ]
 then
      echo "Player1 is Winner"
-elif [ $position2 -eq $WIN_POS ]
+elif [ $posPlayer2 -eq $WIN_POS ]
 then
      echo "Player2 is Winner"
 fi
 }
-gamePlaying="$( checkForOption ) " 
+gamePlaying="$( snakeAndLadderGame ) " 
 
 
