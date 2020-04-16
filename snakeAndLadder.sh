2c2c81744cc960e061e36b179eb88dfe80484bfe
sampada8217 <sampadatirthkar@gmail.com>
2020-04-16 12:08:14 +0530

    Added Code to count no of times dice played to win the game

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,14 +2,15 @@
 echo "Welcome to Snake and Ladder Game"
 WIN_POS=100
 position=0
-rollsDie=$(( (RANDOM%6) +1 ))
-
+count=0
 function  checkForOption()
 {
      while [ $position -ne $WIN_POS ]
      do
-     	option=$(( (RANDOM%3) +1 ))
-     	case $option in
+        rollsDie=$(( (RANDOM%6) +1 ))
+        ((count++))
+  	option=$(( (RANDOM%3) +1 ))
+ 	case $option in
      	1)echo $position                ;;
      	2)position=$(( $position + $rollsDie ))
           if [ $position -gt 100 ] 
@@ -24,6 +25,7 @@ function  checkForOption()
          fi          ;;
      	esac
      done
+echo $count
 }
 checkPosition="$( checkForOption )"
 
