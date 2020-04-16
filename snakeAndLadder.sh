abf89c7a5c83a562a0e5600c8d61dcdd9326fe41
sampada8217 <sampadatirthkar@gmail.com>
2020-04-16 11:53:33 +0530

    Added Code for Ensure Player gets to Exact Winning Position

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -6,12 +6,17 @@ rollsDie=$(( (RANDOM%6) +1 ))
 
 function  checkForOption()
 {
-     while [ $position -le $WIN_POS ]
+     while [ $position -ne $WIN_POS ]
      do
      	option=$(( (RANDOM%3) +1 ))
      	case $option in
-     	1)echo $position            ;;
-     	2)position=$(( $position + $rollsDie )) ;;
+     	1)echo $position                ;;
+     	2)position=$(( $position + $rollsDie ))
+          if [ $position -gt 100 ] 
+          then
+               position=$(( $position - $rollsDie ))
+          fi
+                                  ;;
       	3)position=$(( $position - $rollsDie ))
          if [ $position -lt 0 ]
          then
